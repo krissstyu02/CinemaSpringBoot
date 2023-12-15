e6a1004a0a8963dcacb168a8bf91a0fad6c2f5f2
Кристина <kristi090217@gmail.com>
2023-12-15 11:54:06 +0300

    Initial commit

---
+++ src/main/java/com/example/cinemaspringboot/CinemaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.cinemaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaSpringBootApplication.class, args);
+    }
+
+}
